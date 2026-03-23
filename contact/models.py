de9a92f614cba44953f0b019ea4ebee2d342dc33
oscarlmjr <oscarlmjr@gmail.com>
2026-03-23 14:30:55 -0300

    secao_10 aula_457

--- contact/models.py
+++ contact/models.py
@@ -17,3 +17,6 @@ class Contact(models.Model):
 	email = models.EmailField(max_length=50, blank=True)
 	created_date = models.DateTimeField(default=timezone.now)
 	description = models.TextField(blank=True)
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'

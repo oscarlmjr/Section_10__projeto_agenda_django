4dc29fd392c54454110a12a54120d811260d0cdc
oscarlmjr <oscarlmjr@gmail.com>
2026-03-24 12:10:01 -0300

    secao_10 aula_461

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,12 @@ class ContactAdmin(admin.ModelAdmin):
 	ordering = 'id', 
 	list_per_page = 10
 	list_max_show_all = 200
-	list_filter = 'created_date',
+	# list_filter = 'created_date',
 	search_fields = 'id', 'first_name', 'last_name',
 	list_editable = 'first_name', 'last_name',
 	list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = 'name',
+	ordering = '-id', 
--- contact/models.py
+++ contact/models.py
@@ -10,6 +10,12 @@ from django.utils import timezone
 # owner (foreign key)
 
 
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
 
 class Contact(models.Model):
 	first_name = models.CharField(max_length=50)
@@ -20,6 +26,8 @@ class Contact(models.Model):
 	description = models.TextField(blank=True)
 	show = models.BooleanField(default=True)
 	picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
-
-	def __str__(self):
-		return f'{self.first_name} {self.last_name}'
+	category = models.ForeignKey(
+		Category, 
+		on_delete=models.SET_NULL,
+		blank=True, null=True,
+	)

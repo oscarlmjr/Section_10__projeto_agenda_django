eb97d2508cd8e03b6c87b37429cf5db2f87f9513
oscarlmjr <oscarlmjr@gmail.com>
2026-03-24 15:37:51 -0300

    secao_10 aula_463

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -35,3 +36,11 @@ class Contact(models.Model):
 		on_delete=models.SET_NULL,
 		blank=True, null=True,
 	)
+	owner = models.ForeignKey(
+		User,
+		on_delete=models.SET_NULL,
+		blank=True, null=True
+	)
+
+	def __str__(self) -> str:
+		return f'{self.first_name} {self.last_name}'

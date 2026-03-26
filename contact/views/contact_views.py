061a890ba7acef43f98e53b0ad02e607baae7e65
oscarlmjr <oscarlmjr@gmail.com>
2026-03-25 21:02:59 -0300

    secao_10 aula_473-474

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
 
 from contact.models import Contact
 
@@ -20,6 +20,36 @@ def index(request):
 		context,
 	)
 
+def search(request):
+	search_value = request.GET.get('q', '').strip()
+
+	print(search_value)
+
+	if search_value == '':
+		return redirect('contact:index')
+
+	contacts = Contact.objects \
+		.filter(show=True) \
+		.filter(
+			Q(first_name__icontains=search_value) |
+			Q(last_name__icontains=search_value) |
+			Q(phone__icontains=search_value) |
+			Q(email__icontains=search_value)
+		) \
+		.order_by('-id')
+	
+	print(contacts.query)
+	
+	context = {
+		'contacts': contacts,
+		'site_title': 'Search - '
+	}
+
+	return render(
+		request,
+		'contact/index.html',
+		context,
+	)
 
 def contact(request,contact_id):
 	# single_contact = Contact.objects.get(pk=contact_id).first()
